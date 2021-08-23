520b407fba4ab534f368abd9c97977f0ded28f07
T H <@>
2021-08-23 18:27:55 +0200

    Number of paths between two verties

---
+++ java/src/dattastructures/graph/pathnum/Graph.java
@@ -0,0 +1,82 @@
+// path number between two vertices, directed graph
+package dattastructures.graph.pathnum;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Graph {
+
+	private Integer numOfVertices;
+	private List<List<Integer>> graph = new ArrayList<>();
+	
+	public Graph(int numOfVertices) {
+		this.numOfVertices = numOfVertices;
+		for(int index=0; index < this.numOfVertices; index++) {
+			graph.add(index, new LinkedList<Integer>());
+		}
+	}
+	
+	public void addEdge(Integer vertex, Integer adjacentVertex) {
+		graph.get(vertex).add(adjacentVertex);
+	}
+	
+	private Integer pathUtil(Integer numOfPaths, Integer vertex, Integer endVertex, List<Integer>pathVertices) {
+		if(!pathVertices.contains(vertex)) {
+			pathVertices.add(vertex);
+			if(!vertex.equals(endVertex)) {
+				// Recur for all the vertices
+		        // adjacent to this vertex
+				for(Integer nextVertex: graph.get(vertex)) {
+					if(!pathVertices.contains(nextVertex)) {
+						List<Integer> pathVerticesClone = new ArrayList<>(pathVertices);
+						numOfPaths = pathUtil(numOfPaths, nextVertex, endVertex, pathVerticesClone);
+					}
+				}
+			}else {
+				// If current vertex is same as
+		        // destination, then increment count
+				System.out.println("hit");
+				System.out.println(pathVertices);
+				numOfPaths++;
+			}
+		}
+		return numOfPaths;
+
+	}
+	
+	public Integer getPathNumber(Integer startVertex, Integer endVertex){
+		Integer numOfPaths = 0;
+		List<Integer> pathVertices = new ArrayList<Integer>();
+		return pathUtil(numOfPaths, startVertex, endVertex, pathVertices);
+//		return numOfPaths;
+	}
+	
+	public static void main(String[] args) {
+		
+//	--->0------>1
+//	|   |  \ /> |
+//	|   |   X   |
+//	|  \|  / \  |/
+//	----2      >3<--
+//		        |  |
+//		        ----
+		
+		Graph g = new Graph(4);
+		g.addEdge(0, 1);
+		g.addEdge(0, 2);
+		g.addEdge(0, 3);
+		g.addEdge(2, 0);
+		g.addEdge(2, 1);
+		g.addEdge(1, 3);
+		g.addEdge(3, 3);
+		
+		Integer start = 0;
+		Integer end = 3;
+		Integer num  = g.getPathNumber(start, end);
+		System.out.println(num);
+		System.out.println(String.format("From %d to %d there are %d paths", start,end,num));
+
+	}
+
+}
---
+++ py/datastructuresGraphPathnum.py
@@ -0,0 +1,59 @@
+# Python 3 program to count all paths
+# from a source to a destination in directed graph
+from collections import defaultdict
+
+
+class Graph:
+
+    def __init__(self, num_of_vertices):
+        self.graph = defaultdict(list)
+
+    def add_edge(self, vertex, other_vertex):
+        self.graph[vertex].append(other_vertex)
+
+    def path_num_utils(self, vertex, end_vertex, num_of_paths, path_vertices):
+        if vertex not in path_vertices:
+            path_vertices.append(vertex)
+            if vertex != end_vertex:
+                for next_vertex in self.graph[vertex]:
+                    if next_vertex not in path_vertices:
+                        copy_path_vertices = list(path_vertices)
+                        num_of_paths = self.path_num_utils(next_vertex, end_vertex, num_of_paths, copy_path_vertices)
+            else:
+                # If current vertex is same as
+                # destination, then increment count
+                print('bingo')
+                print(path_vertices)
+                num_of_paths += 1
+
+        return num_of_paths
+
+    def get_path_number(self, start_vertex, end_vertex):
+        num_of_paths = 0
+        path_vertices = []
+        return self.path_num_utils(start_vertex, end_vertex, num_of_paths, path_vertices)
+
+
+if __name__ == '__main__':
+
+    # --->0------>1
+    # |   | \ / > |
+    # |   |  X    |
+    # |  \| /  \  |/
+    # ----2     > 3<--
+    #             |  |
+    #             ----
+
+    g = Graph(4)
+    g.add_edge(0, 1)
+    g.add_edge(0, 2)
+    g.add_edge(0, 3)
+    g.add_edge(2, 0)
+    g.add_edge(2, 1)
+    g.add_edge(1, 3)
+    g.add_edge(3, 3)
+
+    start = 0
+    end = 3
+    pathnum = g.get_path_number(start, end)
+    print(f'Between {start} and {end} there is {pathnum} paths')
